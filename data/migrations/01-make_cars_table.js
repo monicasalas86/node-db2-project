5fd1f32da523ca160bac61a59518fd9e9e6ed0ed
monicasalas86 <monicsalas167@gmail.com>
2021-11-02 16:45:40 -0500

    made cars schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,19 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments('car_id')
+    table.text('vin', 128)
+      .unique().notNullable()
+    table.text('make', 128)
+      .notNullable()
+    table.text('model', 128)
+      .notNullable()
+    table.integer('mileage')
+      .notNullable()
+    table.text('title')
+    table.text('transmission')
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
